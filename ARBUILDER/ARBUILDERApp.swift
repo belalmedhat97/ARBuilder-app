5ed2e88ed9b485a1a5d68391e3e249cca74728f3
Belal <belal1997medhat@gmail.com>
2023-02-04 12:31:26 +0000

    Initial Commit

---
+++ ARBUILDER/ARBUILDERApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARBUILDERApp.swift
+//  ARBUILDER
+//
+//  Created by belal medhat on 04/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ARBUILDERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
